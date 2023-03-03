5a2dab0bcf519e6e4546531c624e787690252064
Andreas Klöker <prroet@gmail.com>
2023-03-03 23:17:36 +0100

    Rewrite lua
    * lsp configuration improvement

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,11 +1,9 @@
 require('configs')
 require('keybindings')
-
 require('mappings')
 require('plugins')
 require('lspconfig-configuration')
 
 vim.cmd([[
-  set guifont=Hack\ Nerd\ Font\ Mono
   autocmd BufWritePre *.tf lua vim.lsp.buf.format()
 ]])
--- config/nvim/lua/configs.lua
+++ config/nvim/lua/configs.lua
@@ -1,35 +1,54 @@
-vim.cmd([[
-  " syntax highlighting
-  filetype on
-  
-  " path for finding files
-  set path+=**
-  " menu for finding
-  set wildmenu
-  
-  " tab is only 2 spaces
-  set tabstop=2
-  "set softtabstop=2
-  set shiftwidth=2
-  retab
-  set expandtab
-  " end of tab settings
-  " set list if we want to see everything
-  set listchars=tab:»»,space:·,eol:$
-  " line numbers.
-  set number relativenumber
-  
-  let g:netrw_banner = 0
-  let g:netrw_liststyle = 3
-  let g:netrw_browse_split = 4
-  let g:netrw_altv = 1
-  " c-tab
-  let g:netrw_usetab = 1
-  let g:netrw_winsize = 10
-  
-  " enable matchit package for braces matching
-  " packadd! matchit
-  
+-- font
+vim.opt.guifont = 'Hack Nerd Font Mono'
+
+--syntax highlighting
+vim.cmd.filetype('on')
+
+-- path for finding files
+vim.opt.path:append { "**" }
+
+-- set tab 2 spaces
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.cmd.retab()
+-- <TAB> are spaces
+vim.opt.expandtab = true
+-- end of tab settings
+
+-- set list if we want to see everything
+vim.opt.listchars = { tab = '»»' ,space = '·',eol = '$' }
+
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+local function netrw()
+  local var netrw_settings = {
+    banner = {
+      no = 0
+    },
+    liststyle = {
+      tree = 3
+    },
+    browse_split = {
+      previous_window = 4
+    }
+  }
+  -- netrw settings
+  -- no annoying banner
+  vim.g.netrw_banner = netrw_settings.banner.no
+  vim.g.netrw_liststyle = netrw_settings.liststyle.tree
+  vim.g.netrw_browse_split = netrw_settings.browse_split.previous_window
+  -- vim.g.netrw_altv = 1
+  vim.g.netrw_usetab = 1
+  vim.g.netrw_winsize = 10
+end
+
+netrw()
+
+vim.cmd([[   
   " highlight trailing spaces red
   " https://stackoverflow.com/questions/48935451/how-do-i-get-vim-to-highlight-trailing-whitespaces-while-using-vim-at-the-same-t
   highlight RedundantSpaces ctermbg=red guibg=red
